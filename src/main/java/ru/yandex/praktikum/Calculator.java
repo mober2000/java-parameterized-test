4c5b3c524fa90a96a626683bed0c489faf18d973
Alexey Chebanov <chebik2012@gmail.com>
2022-09-18 15:11:27 +0300

    "My parameterized test"

---
+++ src/main/java/ru/yandex/praktikum/Calculator.java
@@ -0,0 +1,12 @@
+package ru.yandex.praktikum;
+
+public class Calculator {
+
+    public int sum(int a, int b) {
+        return a + b;
+    }
+
+    public int mult(int a, int b) {
+        return a * b;
+    }
+}
---
+++ src/test/java/ru/yandex/praktikum/CalculatorTest.java
@@ -0,0 +1,18 @@
+package ru.yandex.praktikum;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class CalculatorTest extends Calculator {
+
+    @Test
+    public void sum(){
+    }
+
+    @Test
+    public void mult(){
+    }
+}
